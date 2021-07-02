e49f9859850b70e233b27665472768f46467da0b
Abby Heiser <85967450+aheiser2@users.noreply.github.com>
2021-07-02 00:48:24 -0500

    Create WorkOutCode.js

---
+++ WorkOutCode.js
@@ -0,0 +1,13 @@
+var sitUps = 0 
+var pushUps = 0 
+var plank = 0 
+
+ for(monthDate = 1; monthDate <= 30; monthDate++){  
+   plank += 5; 
+   pushUps += 2; 
+   sitUps += 2; 
+   console.log("On day " + monthDate + " do:"); 
+   console.log(plank + " sec plank"); 
+   console.log(pushUps + " pushups"); 
+   console.log(sitUps + " sitUps"); 
+} 
